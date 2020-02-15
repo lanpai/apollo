527b4b83d052b94b2b369920959c86bbe99f4c84
Tony Yeon <tony.yeon8310@gmail.com>
2020-02-15 14:58:22 -0800

    movement and camera follow

---
+++ apollo/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTransform;
+
+    public float cameraEase;
+    public Vector3 cameraOffset;
+    
+    private Camera camera;
+
+    private void Start() {
+        camera = GetComponent<Camera>();
+    }
+    private void FixedUpdate() {
+        transform.position = Vector3.Lerp(transform.position, playerTransform.position + cameraOffset, cameraEase * Time.deltaTime);
+    }
+}
