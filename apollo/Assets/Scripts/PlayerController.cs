9d8ca4e9b2357d34ffb0ee4522a33e2a8ee6f2d5
Tony Yeon <tony.yeon8310@gmail.com>
2020-02-15 20:12:52 -0800

    player rotation prototype

--- apollo/Assets/Scripts/PlayerController.cs
+++ apollo/Assets/Scripts/PlayerController.cs
@@ -12,31 +12,59 @@ public class PlayerController : MonoBehaviour
 
     public float rotationSpeed;
 
-    private void Update() {
+    private void FixedUpdate() {
 
         Vector3 finalMovement = Vector3.zero;
+        float currentRotation = player.transform.rotation.eulerAngles.y;
 
-        if(Input.GetKey("w"))
+        print(currentRotation);
+
+        if (Input.GetKey("w"))
         {
             finalMovement.z += 1;
-        }
-        if(Input.GetKey("a"))
-        {
-            finalMovement.x -= 1;
-        }
-        if(Input.GetKey("s"))
-        {
-            finalMovement.z -= 1;
-        }
-        if(Input.GetKey("d"))
-        {
-            finalMovement.x += 1;
-            if(player.transform.rotation.eulerAngles.y <= 90)
+            if (currentRotation > 180)
             {
-                player.transform.Rotate(Vector3.ClampMagnitude(new Vector3(0, rotationSpeed, 0), 90), Space.Self);
+                ccwRotation();
+            }
+            else {
+                cwRotation();
             }
         }
-        if(Input.GetKeyDown("q"))
+        if (Input.GetKey("a"))
+        {
+            finalMovement.x -= 1;
+            if (currentRotation < 270 && currentRotation > 90)
+            {
+                ccwRotation();
+            }
+            else {
+                cwRotation();
+            }
+            
+        }
+        if (Input.GetKey("s"))
+        {
+            finalMovement.z -= 1;
+            if (currentRotation > 0 && currentRotation < 180)
+            {
+                ccwRotation();
+            }
+            else {
+                cwRotation();
+            }
+        }
+        if (Input.GetKey("d"))
+        {
+            finalMovement.x += 1;
+            if (currentRotation > 270 || currentRotation < 90)
+            {
+                ccwRotation();
+            }
+            else {
+                cwRotation();
+            }
+        }
+        if (Input.GetKeyDown("q"))
         {
             GetComponent<PlayerInteractions>().dropItem(player.transform);
         }
@@ -44,5 +72,13 @@ public class PlayerController : MonoBehaviour
         player.AddForce(speed * finalMovement.normalized * Time.deltaTime);
         player.velocity = Vector3.ClampMagnitude(player.velocity, speedLimit);
         
+    }
+    private void cwRotation()
+    {
+        player.transform.Rotate(Vector3.ClampMagnitude(new Vector3(0, rotationSpeed, 0), 90) * Time.deltaTime, Space.Self);
+    }
+    private void ccwRotation()
+    {
+        player.transform.Rotate(Vector3.ClampMagnitude(new Vector3(0, -rotationSpeed, 0), 90) * Time.deltaTime, Space.Self);
     }
 }
