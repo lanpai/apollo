0f395699641caa6d614636801b42a67a1a7d1477
Tony Yeon <tony.yeon8310@gmail.com>
2020-02-15 18:11:05 -0800

    dropitem

--- apollo/Assets/Scripts/PlayerInteractions.cs
+++ apollo/Assets/Scripts/PlayerInteractions.cs
@@ -8,9 +8,11 @@ public class PlayerInteractions : MonoBehaviour
 
     public void dropItem(Transform player)
     {
-        print("dropitem");
-        GameObject item = Instantiate(itemPrefab) as GameObject;
-        item.transform.position = player.position;
-        item.GetComponent<Rigidbody>().velocity += Vector3.forward;
+        float playerRadius = player.GetComponent<CapsuleCollider>().radius;
+        float playerHeight = player.GetComponent<CapsuleCollider>().height;
+
+        Vector3 itemDropOffset = (player.transform.position - player.transform.forward * playerRadius * 1.5f) + (0.5f * new Vector3(0, playerHeight, 0));
+
+        GameObject item = Instantiate(itemPrefab, itemDropOffset, transform.rotation) as GameObject;
     }
 }
