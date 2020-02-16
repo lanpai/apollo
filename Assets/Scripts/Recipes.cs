f0832f1bef58e6e397e6c97372928273243dd000
Baron-Paelen <dawnplaya55@gmail.com>
2020-02-16 07:04:53 -0800

    Finished recipe list and some balancing

--- Assets/Scripts/Recipes.cs
+++ Assets/Scripts/Recipes.cs
@@ -5,28 +5,77 @@ using UnityEngine;
 public class Recipes : MonoBehaviour
 {
 
-    public static Dictionary<ItemType, List<ItemType>> recipes = new Dictionary<ItemType, List<ItemType>>();
+    public static Dictionary<List<ItemType>, List<ItemType>> recipes = new Dictionary<List<ItemType>, List<ItemType>>();
 
     static Recipes() 
     {
-        recipes.Add(ItemType.Radio, new List<ItemType>{ 
-            ItemType.Radio});
+        recipes.Add( //radio
+            new List<ItemType> { ItemType.Copper, ItemType.Lithium, ItemType.Gold, ItemType.Aluminum }, //output
+            new List<ItemType> { ItemType.Radio}); //input
+
+        recipes.Add( //pillow
+            new List<ItemType> { ItemType.Carbon,  ItemType.Hydrogen, ItemType.Hydrogen, ItemType.Oxygen},
+            new List<ItemType> { ItemType.Pillow });
+
+        recipes.Add( //flag
+            new List<ItemType> { ItemType.Carbon, ItemType.Carbon, ItemType.Carbon, ItemType.Hydrogen, ItemType.Hydrogen, ItemType.Oxygen },
+            new List<ItemType> { ItemType.Flag });
+
+        recipes.Add( //laptop
+            new List<ItemType> { ItemType.Carbon, ItemType.Carbon, ItemType.Aluminum, ItemType.Germanium, ItemType.Silicon, ItemType.Silicon },
+            new List<ItemType> { ItemType.Laptop });
+
+        recipes.Add( //seats
+            new List<ItemType> { ItemType.Carbon, ItemType.Carbon, ItemType.Hydrogen, ItemType.Hydrogen },
+            new List<ItemType> { ItemType.Seats });
+
+        recipes.Add( //Inner Wall
+            new List<ItemType> { ItemType.Aluminum, ItemType.Aluminum, ItemType.Titanium, ItemType.Titanium },
+            new List<ItemType> { ItemType.InnerWall });
+
+        recipes.Add( //mattress
+            new List<ItemType> { ItemType.Carbon, ItemType.Hydrogen },
+            new List<ItemType> { ItemType.Mattress });
+
+        recipes.Add( //oven
+            new List<ItemType> { ItemType.Iron, ItemType.Iron, ItemType.Iron, ItemType.Carbon, ItemType.Carbon, ItemType.Copper },
+            new List<ItemType> { ItemType.Oven });
+
+        recipes.Add( //clothing
+            new List<ItemType> { ItemType.Carbon, ItemType.Carbon, ItemType.Hydrogen, ItemType.Carbon, ItemType.Carbon },
+            new List<ItemType> { ItemType.Clothing });
+
+        recipes.Add( //toilet
+            new List<ItemType> { ItemType.Silicon, ItemType.Oxygen, ItemType.Oxygen, ItemType.Aluminum },
+            new List<ItemType> { ItemType.Toilet });
+
+        recipes.Add( //book
+            new List<ItemType> { ItemType.Carbon, ItemType.Oxygen, ItemType.Hydrogen },
+            new List<ItemType> { ItemType.Book });
+
+        recipes.Add( //cell phone
+            new List<ItemType> { ItemType.Aluminum, ItemType.Lithium, ItemType.Silicon, ItemType.Nickel, ItemType.Gold },
+            new List<ItemType> { ItemType.CellPhone });
+
+        recipes.Add( //water bottle
+            new List<ItemType> { ItemType.Iron, ItemType.Carbon, ItemType.Carbon, ItemType.Hydrogen, ItemType.Hydrogen },
+            new List<ItemType> { ItemType.WaterBottle });
+
+        recipes.Add( //utensil
+            new List<ItemType> { ItemType.Carbon, ItemType.Hydrogen },
+            new List<ItemType> { ItemType.Utensil });
+
+        recipes.Add( //server
+            new List<ItemType> { ItemType.Carbon, ItemType.Carbon, ItemType.Carbon, ItemType.Carbon, ItemType.Aluminum, ItemType.Aluminum, ItemType.Germanium, ItemType.Silicon, ItemType.Silicon, ItemType.Silicon, ItemType.Silicon },
+            new List<ItemType> { ItemType.Server });
     }
-    public static Dictionary<ItemType, List<ItemType>> getDictionary()
-    {
-        return recipes;
-    }
 
-    public static List<ItemType> CheckRecipes(List<ItemType> items) {
-        List<ItemType> results = new List<ItemType>();
+    public static List<List<ItemType>> CheckRecipes(List<ItemType> items) {
+        List<List<ItemType>> results = new List<List<ItemType>>();
 
-        Debug.Log(recipes.Count);
+        Debug.Log("check");
 
-        foreach (KeyValuePair<ItemType, List<ItemType>> i in recipes) {
-
-            Debug.Log(i.Key);
-            Debug.Log(i.Value);
-            
+        foreach (KeyValuePair<List<ItemType>, List<ItemType>> i in recipes) {
             bool recipeValid = true;
             foreach (ItemType requirement in i.Value) {
                 bool reqFound = false;
--- Assets/Scripts/TriggerSynthesisController.cs
+++ Assets/Scripts/TriggerSynthesisController.cs
@@ -4,40 +4,25 @@ using UnityEngine;
 
 public class TriggerSynthesisController : MonoBehaviour
 {
-    public List<Collider> elements = new List<Collider>();
-    public Dictionary<ItemType, List<ItemType>> recipes;
     public List<ItemType> items = new List<ItemType>();
 
-    private void Start() {
-        recipes = Recipes.getDictionary();
-    }
-    private void Update() 
-    {
-        foreach (Collider i in elements)
-        {
-            if (i == null)
-            {
-                elements.Remove(i);
-            }
-        }
-    }
     private void OnTriggerEnter(Collider other) {
         items.Add((other.gameObject.GetComponent(typeof(Item)) as Item).getType());
         Destroy(other.gameObject);
 
-        List<ItemType> validRecipes = Recipes.CheckRecipes(items);
+        List<List<ItemType>> validRecipes = Recipes.CheckRecipes(items);
         if (validRecipes.Count == 1) {
-            Debug.Log(validRecipes[0]);
-
-            switch (validRecipes[0]) {
-                case ItemType.Radio:
-                    GameObject prefab = Resources.Load("Prefabs/Radio") as GameObject;
-                    Instantiate(prefab, Vector3.zero + Vector3.up * 5.0f, Quaternion.identity);
-                    break;
+            foreach (ItemType type in validRecipes[0]) {
+                switch (type) {
+                    case ItemType.Radio:
+                        GameObject prefab = Resources.Load("Prefabs/Radio") as GameObject;
+                        Instantiate(prefab, Vector3.zero + Vector3.up * 5.0f, Quaternion.identity);
+                        break;
+                }
             }
+            items.Clear();
         }
-        else if (validRecipes.Count == 0)
-        {
+        else if (validRecipes.Count == 0) {
             Debug.Log("No Recipes Found!");
         }
     }
