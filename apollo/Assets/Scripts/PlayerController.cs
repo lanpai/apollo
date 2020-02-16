1cb58b1da6e13cdbe98a3dade534b09174f07455
lanpai <lan@piyo.cafe>
2020-02-15 17:00:32 -0800

    Updated to newest Unity version; Added apollo.fbx and rig + materials

--- apollo/Assets/Scripts/PlayerController.cs
+++ apollo/Assets/Scripts/PlayerController.cs
@@ -16,33 +16,19 @@ public class PlayerController : MonoBehaviour
 
         Vector3 finalMovement = Vector3.zero;
 
-        if(Input.GetKey("w"))
-        {
+        if (Input.GetKey("w"))
             finalMovement.z += 1;
-        }
-        if(Input.GetKey("a"))
-        {
+        if (Input.GetKey("a"))
             finalMovement.x -= 1;
-        }
-        if(Input.GetKey("s"))
-        {
+        if (Input.GetKey("s"))
             finalMovement.z -= 1;
-        }
-        if(Input.GetKey("d"))
-        {
+        if (Input.GetKey("d"))
             finalMovement.x += 1;
-            if(player.transform.rotation.eulerAngles.y <= 90)
-            {
-                player.transform.Rotate(Vector3.ClampMagnitude(new Vector3(0, rotationSpeed, 0), 90), Space.Self);
-            }
-        }
-        if(Input.GetKey("q"))
-        {
+
+        if (Input.GetKey("q"))
             GetComponent<PlayerInteractions>().dropItem(player.transform);
-        }
 
         player.AddForce(speed * finalMovement.normalized * Time.deltaTime);
         player.velocity = Vector3.ClampMagnitude(player.velocity, speedLimit);
-        
     }
 }
