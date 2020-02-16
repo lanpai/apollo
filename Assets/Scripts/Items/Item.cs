53b3f278f64ed96bd533dc1dc07028a5cd15b56e
Baron-Paelen <dawnplaya55@gmail.com>
2020-02-15 21:21:26 -0800

    All Preliminary Items
    Added all item classes from the doc. Haven't fully fleshed out all items.

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Item
+{
+    public string Description { get; }
+    public int Count { get; set; }
+}
